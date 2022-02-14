d2d1db77a79c988a3729ff171a0a83419ff45bea
ethan-jones <egj77508@uga.edu>
2022-02-13 23:32:05 -0500

    Added select all and deselect all buttons

--- randomizer.py
+++ randomizer.py
@@ -1,21 +1,40 @@
+from distutils import command
 import tkinter as tk
 from tkinter import *
 import characters
+import random
 
 window = tk.Tk()
-chrValues = [tk.IntVar()] * 34
+chrValues = []
+for i in range(34):
+    chrValues.append(tk.IntVar())
+    
+enabledCharacters = []
+chrBoxes = []
 
 
 window.title("The Binding of Isaac: Repentance Randomizer")
 window.resizable(width=False, height=False)
 
+
+
 # Functions
 def randomizeCharacters():
-    print("Test")
-
-def confirmSelection():
-    print("Test")
+    enabledCharacters.clear()
+    for i in range(34):
+        if (chrValues[i].get() == 1):
+            enabledCharacters.append(characters.list[i])
+    print(random.choice(enabledCharacters).name)
+ 
+def selectAll():
+    for chr in chrBoxes:
+        chr.select()
         
+def deselectAll():
+    for chr in chrBoxes:
+        chr.deselect()
+
+
 
 canvas = tk.Canvas(window, height=800, width=800, bg="#E9E7DF")
 canvas.pack()
@@ -25,19 +44,25 @@ checkBoxes.place(relwidth=0.825, relheight=0.825, relx=0.08, rely=0.05)
 
 i = 0
 j = 0
+val = 0
 for chr in characters.list:
     if (i == 16):
         i = 0
         j = 1
-    tk.Checkbutton(checkBoxes, text=chr.name, variable=chrValues[i],
-                   onvalue=1, offvalue=0, bg="white").grid(row=i, column=j, sticky=W)
+    chrBoxes.append(tk.Checkbutton(checkBoxes, text=chr.name, variable=chrValues[val],
+                   onvalue=1, offvalue=0, bg="white"))
+    chrBoxes[val].grid(row=i, column=j, sticky=W)
     i = i + 1
+    val = val + 1
      
+     
+selectAllButton = tk.Button(checkBoxes, text="Select All", command=selectAll)
+selectAllButton.grid(row=17, column=0, sticky=E)
 
-confirm = tk.Button(window, text="Confirm Selection")
-confirm.pack()
-
-randomize = tk.Button(window, text="Randomize")
+deselectAllButton = tk.Button(checkBoxes, text="Deselect All", command=deselectAll)
+deselectAllButton.grid(row=17, column=1, sticky=W)
+     
+randomize = tk.Button(window, text="Randomize", command=randomizeCharacters)
 randomize.pack()
 
 window.mainloop()

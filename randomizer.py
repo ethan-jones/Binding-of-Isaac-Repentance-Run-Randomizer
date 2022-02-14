23d43430ba1b928caa4aa90489d04d098ce39777
ethan-jones <egj77508@uga.edu>
2022-02-13 20:46:33 -0500

    Added check box for testing

--- randomizer.py
+++ randomizer.py
@@ -9,5 +9,12 @@ window.resizable(width=False, height=False)
 canvas = tk.Canvas(window, height=800, width=800, bg="#E9E7DF")
 canvas.pack()
 
+checkBoxes = tk.Frame(window, bg="white")
+checkBoxes.place(relwidth=0.825, relheight=0.825, relx=0.05, rely=0.05)
+
+testVar = tk.IntVar()
+check = tk.Checkbutton(checkBoxes, text="Test Box",
+                       variable=testVar, onvalue=1, offvalue=0)
+check.grid(row=0)
 
 window.mainloop()
